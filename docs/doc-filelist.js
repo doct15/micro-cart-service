c456048212f43fda312136217262c64ebd36f78a
Nicolás Cornaglia <ncornag@gmail.com>
2016-05-18 09:59:16 +0200

    Added code documentation.

---
+++ docs/doc-filelist.js
@@ -0,0 +1,1 @@
+var tree={"files":["index.js"],"dirs":{"modules":{"dirs":{"cart":{"dirs":{"hooks":{"files":["addToCart.js","postAddToCart.js","postSaveCart.js","preAddToCart.js","saveCart.js","stockAvailability.js"]},"models":{"files":["cartModel.js"]},"schemas":{"files":["addEntrySchema.js"]}},"files":["index.js"]}}}}};
